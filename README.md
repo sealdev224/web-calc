b24369cd35aeac9ec9982a4b4d717e8d3414207f
sealdev224 <sealdev224@gmail.com>
2026-05-16 09:17:30 -0500

    more stuff like basic js

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # web-calc
 
 Just for a first repo on github I wanna make a calculator on this new account so yeah :D "aka I lost my 6 year old github account :("
+
+no tutroijfals followewd becuase im just like that ofc :>
---
+++ script.js
@@ -0,0 +1,25 @@
+let display = document.getElementById('display');
+let deleteBtn = document.getElementById('delete');
+let resetBtn = document.getElementById('reset');
+let averageBtn = document.getElementById('average');
+let divideBtn = document.getElementById('divide');
+let sevenBtn = document.getElementById('seven');
+let eightBtn = document.getElementById('eight');
+let nineBtn = document.getElementById('nine');
+let multiplyBtn = document.getElementById('multiply');
+let fourBtn = document.getElementById('four');
+let fiveBtn = document.getElementById('five');
+let sixBtn = document.getElementById('six');
+let subtractBtn = document.getElementById('subtract');
+let oneBtn = document.getElementById('one');
+let twoBtn = document.getElementById('two');
+let threeBtn = document.getElementById('three');
+let addBtn = document.getElementById('add');
+let fillerBtn = document.getElementById('filler');
+let zeroBtn = document.getElementById('zero');
+let decimalBtn = document.getElementById('decimal');
+let equalsBtn = document.getElementById('equals');
+
+let currentInput = '';
+// me writing this js rn crying knowing its gonna be the biggest mess known to mankind :(
+
